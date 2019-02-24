781402d027cc779f9261b26f55aa20302ac6703d
Pranav Vashi <neobuddy89@gmail.com>
2019-02-24 17:16:23 +0530

    Kill redundant search fun party
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/org/lineageos/lineageparts/contributors/ContributorsCloudFragment.java
+++ src/org/lineageos/lineageparts/contributors/ContributorsCloudFragment.java
@@ -60,9 +60,6 @@ import android.widget.TextView;
 
 import org.lineageos.lineageparts.PartsActivity;
 import org.lineageos.lineageparts.R;
-import org.lineageos.lineageparts.search.BaseSearchIndexProvider;
-import org.lineageos.lineageparts.search.SearchIndexableRaw;
-import org.lineageos.lineageparts.search.Searchable;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -75,7 +72,7 @@ import java.util.List;
 import java.util.Locale;
 
 public class ContributorsCloudFragment extends Fragment implements SearchView.OnQueryTextListener,
-        SearchView.OnCloseListener, MenuItem.OnActionExpandListener, Searchable {
+        SearchView.OnCloseListener, MenuItem.OnActionExpandListener {
 
     private static final String TAG = "ContributorsCloud";
 
@@ -807,45 +804,4 @@ public class ContributorsCloudFragment extends Fragment implements SearchView.On
             }
         }
     }
-
-    public static final Searchable.SearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
-            new BaseSearchIndexProvider() {
-
-                @Override
-                public List<SearchIndexableRaw> getRawDataToIndex(Context context) {
-
-                    // Index the top 100 contributors, for fun :)
-                    File dbPath = context.getDatabasePath(DB_NAME);
-                    SQLiteDatabase db = null;
-                    try {
-                        db = SQLiteDatabase.openDatabase(dbPath.getAbsolutePath(),
-                                null, SQLiteDatabase.OPEN_READONLY);
-                        if (db == null) {
-                            Log.e(TAG, "Cannot open cloud database: " + DB_NAME + ". db == null");
-                            return null;
-                        }
-                    } catch (Exception e) {
-                        Log.e(TAG, e.getMessage(), e);
-                        if (db != null && db.isOpen()) {
-                            db.close();
-                        }
-                        return null;
-                    }
-
-                    List<SearchIndexableRaw> result = new ArrayList<>();
-                    Cursor c = db.rawQuery(
-                            "select id, username from metadata order by commits desc limit 100;", null);
-                    while (c.moveToNext()) {
-                        SearchIndexableRaw raw = new SearchIndexableRaw(context);
-                        raw.key = KEY_PREFIX + c.getString(0);
-                        raw.rank = 10;
-                        raw.title = c.getString(1);
-                        result.add(raw);
-                    }
-                    c.close();
-                    db.close();
-
-                    return result;
-                }
-            };
 }
