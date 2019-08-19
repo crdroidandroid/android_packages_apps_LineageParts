5db475974c63584f9715a6dcff2ab4d21d618b19
Pranav Vashi <neobuddy89@gmail.com>
2019-08-20 00:55:19 +0530

    stats: Mod version switched to crdroid version
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/org/lineageos/lineageparts/lineagestats/Utilities.java
+++ src/org/lineageos/lineageparts/lineagestats/Utilities.java
@@ -68,7 +68,7 @@ public class Utilities {
     }
 
     public static String getModVersion() {
-        return SystemProperties.get("ro.lineage.version", Build.DISPLAY);
+        return SystemProperties.get("ro.crdroid.build.version", Build.DISPLAY);
     }
 
     public static String digest(String input) {
