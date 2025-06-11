88936bf08edd237dd628eda0dc5e8b5e60d7df2a
iusmac <iusico.maxim@libero.it>
2025-06-11 18:34:06 +0200

    LineageParts: Do not restore keydisabler state at boot
    Otherwise it would overwrite the user's preference set via the "Disable
    HW keys" setting.
    
    Signed-off-by: iusmac <iusico.maxim@libero.it>

--- src/org/lineageos/lineageparts/BootReceiver.java
+++ src/org/lineageos/lineageparts/BootReceiver.java
@@ -41,11 +41,11 @@ public class BootReceiver extends BroadcastReceiver {
 
         if (!hasRestoredTunable(ctx)) {
             /* Restore the hardware tunable values */
-            ButtonSettings.restoreKeyDisabler(ctx);
+            /* ButtonSettings.restoreKeyDisabler(ctx); */
             setRestoredTunable(ctx);
         }
 
-        ButtonSettings.restoreKeyDisabler(ctx);
+        /* ButtonSettings.restoreKeyDisabler(ctx); */
         ButtonSettings.restoreKeySwapper(ctx);
         TouchscreenGestureSettings.restoreTouchscreenGestureStates(ctx);
     }
